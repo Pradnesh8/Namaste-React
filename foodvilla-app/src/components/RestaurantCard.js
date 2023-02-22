c677f5f543c19915e5444b4bf6b74039dd86c612
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-02-23 00:14:33 +0530

    rating added

--- foodvilla-app/src/components/RestaurantCard.js
+++ foodvilla-app/src/components/RestaurantCard.js
@@ -1,19 +1,25 @@
 import { Link } from "react-router-dom";
 import { CDN_IMG_URL } from "../config";
-
+import starIcon from "./../../assets/star-icon.png";
 // Object is props received from parent and destructuring it to get values
 const RestaurantCard = ({
     name,
     cloudinaryImageId,
     cuisines,
-    slaString
+    slaString,
+    avgRating
 }) => {
     return (
         <div className="h-fit w-72 shadow-sm">
             <img className="h-56 w-[100%] object-cover object-center" src={`${CDN_IMG_URL}${cloudinaryImageId}`} alt="restauarant image" />
-            <h2 className="text-xl font-medium pt-2 pl-1">{name}</h2>
-            <h3 className="pl-1 font-normal">{cuisines.join(", ")}</h3>
-            <h4 className="text-sm pl-1 font-thin pb-2">{slaString}</h4>
+            <div className="px-1 pb-2">
+                <h2 className="text-xl font-medium pt-2 pl-1">{name}</h2>
+                <h3 className="pl-1 font-normal">{cuisines.join(", ")}</h3>
+                <div className="flex items-center gap-2">
+                    <h4 className="flex items-center gap-1"><img src={starIcon} alt="star-icon" className="h-5" /> {avgRating}</h4>
+                    <h4 className="text-base font-thin">{slaString} </h4>
+                </div>
+            </div>
         </div>
     )
 }
