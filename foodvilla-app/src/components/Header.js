2f1f983d1f5982b307e090a918050084c67047b6
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-02-23 03:01:40 +0530

    Added tailwind icons

--- foodvilla-app/src/components/Header.js
+++ foodvilla-app/src/components/Header.js
@@ -27,11 +27,35 @@ const Header = () => {
         <div className="flex px-6 justify-between items-center shadow-md bg-gradient-to-r from-[#A1FFCE] to-[#FAFFD1]">
             <Link to={"/"}><div className='font-logo text-5xl p-4' data-testid="logo">FoodieWoodie</div></Link>
             <ul className='flex gap-10'>
-                <Link to={"/"}><li className='nav-item'>Home</li></Link>
-                <Link to={"/about"}><li className='nav-item'>About</li></Link>
-                <Link to={"/contact"}><li className='nav-item'>Contact</li></Link>
+                <Link to={"/"}><li className='nav-item flex gap-1 items-end'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                        <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                    </svg>
+                    <span>Home</span>
+                </li></Link>
+                <Link to={"/about"}><li className='nav-item flex gap-1 items-end'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clipRule="evenodd" />
+                    </svg>
+                    <span>About</span>
+                </li></Link>
+                <Link to={"/contact"}><li className='nav-item flex gap-1 items-end'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd" />
+                    </svg>
+                    <span>Contact</span>
+                </li></Link>
                 <Link to={"/cart"}>
-                    <li className='nav-item' data-testid="cart-items">Cart {cartItemCount > 0 && `${cartItemCount} - items `}</li>
+                    <li className='nav-item flex gap-1 items-center' data-testid="cart-items">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                            <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                        </svg>
+                        <span>
+                            Cart
+                        </span>
+                        {cartItemCount > 0 && <span className='h-6 w-6 bg-black text-white rounded-full flex items-center justify-center'>{cartItemCount} </span>}
+                    </li>
                 </Link>
                 {
                     UserContext?.user?.name ?
@@ -51,8 +75,13 @@ const Header = () => {
                             </span>
                         </li> :
                         <Link to={"/login"}>
-                            <li className='nav-item'>
-                                Login
+                            <li className='nav-item flex gap-1 items-center'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                    <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+                                </svg>
+                                <span>
+                                    Login
+                                </span>
                             </li>
                         </Link>
                 }
