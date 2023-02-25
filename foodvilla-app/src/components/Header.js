a2e0ee8d250e2c173b2cb2c7fd4fe6d5cfb1f756
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-02-26 00:25:14 +0530

    Login updated with test creds and UI changes

--- foodvilla-app/src/components/Header.js
+++ foodvilla-app/src/components/Header.js
@@ -59,16 +59,16 @@ const Header = () => {
                 </Link>
                 {
                     UserContext?.user?.name ?
-                        <li className='nav-item cursor-pointer flex gap-2 items-center' onClick={() => {
-                            setUser({});
-                        }}>
+                        <li className='nav-item cursor-pointer flex gap-2 items-center'>
                             <span>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
                                 </svg>
                             </span>
                             <span className='mr-2'>{user?.name}</span>
-                            <span>
+                            <span onClick={() => {
+                                setUser({});
+                            }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
                                 </svg>
--- foodvilla-app/src/components/Login.js
+++ foodvilla-app/src/components/Login.js
@@ -1,8 +1,8 @@
 
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import userContext from '../utils/userContext';
 
 const SignInSchema = Yup.object().shape({
@@ -15,10 +15,12 @@ const SignInSchema = Yup.object().shape({
 
 export const Login = () => {
     const UserContext = useContext(userContext);
+    const [testCreds, setTestCreds] = useState({ email: "", password: "" });
+    const [error, setError] = useState("");
     const { user, setUser } = UserContext;
     console.log("UserContext", UserContext);
     const navigate = useNavigate();
-    const handleSubmit = (data) => {
+    const handleSubmit = (data, resetForm) => {
         const { email, password } = data;
         if (email === "test@gmail.com" && password === "admin123") {
             // localStorage.setItem("isLoggedIn", true);
@@ -26,39 +28,62 @@ export const Login = () => {
                 email: "test@gmail.com",
                 name: "Test"
             })
-            navigate(`/`);
+            navigate(-1);
         } else {
-            alert("Incorrect Email or Password");
+            setError("Incorrect Email or Password");
+            resetForm();
+            setTestCreds({ email: "", password: "" })
+            setTimeout(() => {
+                setError("");
+            }, 3000)
         }
     }
+    const insertTestCreds = () => {
+        setTestCreds({ email: "test@gmail.com", password: "admin123" })
+    }
     return (
-        <div className='flex justify-center items-center h-[100vh] flex-col'>
-            <h1 className='font-medium text-2xl'>Login</h1>
+        <div className='flex justify-center items-center h-[100vh] flex-col bg-gradient-to-r from-[#A1FFCE] to-[#FAFFD1]"'>
+            <Link to={"/"}><div className='font-logo text-5xl p-4' data-testid="logo">FoodieWoodie</div></Link>
+            <h1 className='font-medium text-2xl mb-4'>Login</h1>
             <Formik
                 initialValues={{
-                    email: '',
-                    password: '',
+                    email: testCreds?.email,
+                    password: testCreds?.password
                 }}
                 validationSchema={SignInSchema}
-                onSubmit={values => {
+                onSubmit={(values, { resetForm }) => {
                     // same shape as initial values
                     console.log(values);
-                    handleSubmit(values);
+                    handleSubmit(values, resetForm);
                 }}
+                enableReinitialize
             >
                 {({ errors, touched }) => (
-                    <Form className='flex justify-center items-center flex-col w-full'>
-                        <Field className="border p-2 rounded-sm m-2 w-1/3" name="email" type="email" placeholder="Email" />
-                        <div className="errMsg">
-                            <ErrorMessage className='text-red-200' name="email" />
-                        </div>
-                        <Field className="border p-2 rounded-sm m-2 w-1/3" name="password" type="password" placeholder="Password" />
-                        <div className="errMsg">
-                            <ErrorMessage className='text-red-200' name="password" />
-                        </div>
-                        <button type="submit" className='border bg-gray-200 p-2 rounded-md'>Submit</button>
-                    </Form>
-
+                    <>
+                        <Form className='flex justify-center items-center flex-col w-full'>
+                            <Field className="border p-2 rounded-md my-3 w-1/3" name="email" type="email" placeholder="Email" />
+                            <div className="errMsg">
+                                <ErrorMessage className='text-red-200' name="email" />
+                            </div>
+                            <Field className="border p-2 rounded-md my-3 w-1/3" name="password" type="password" placeholder="Password" />
+                            <div className="errMsg">
+                                <ErrorMessage className='text-red-200' name="password" />
+                            </div>
+                            <button type="submit" className='text-base border p-2 my-3 rounded-md bg-[#FAFFD1]'>Submit</button>
+                            <button type="button" onClick={insertTestCreds} className='text-base border p-2 rounded-md bg-[#FAFFD1]'>Use test credentials</button>
+                            {
+                                error?.length > 0 &&
+                                <div className="flex mt-3 p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                                    <svg aria-hidden="true" className="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                                    <span className="sr-only">Info</span>
+                                    <div>
+                                        <span className="font-medium">{error} !</span> Please try with correct credentials.
+                                    </div>
+                                </div>
+                            }
+                        </Form>
+                        {console.log(errors)}
+                    </>
                 )}
             </Formik>
         </div>
