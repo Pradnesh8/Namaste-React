e61c3d87f3ac0e46c1a517f979c5f45b5f4f5653
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-02-28 02:23:30 +0530

    minor bug fix for price

--- foodvilla-app/src/components/Cart.js
+++ foodvilla-app/src/components/Cart.js
@@ -42,7 +42,7 @@ const Cart = () => {
     }
 
     const calculatePrice = () => {
-        const price = cartItems.reduce((sum, item) => sum + item.count * (item.price / 100), 0);
+        const price = cartItems.reduce((sum, item) => sum + item.count * (item.price === 0 ? item.defaultPrice / 100 : item.price / 100), 0);
         return parseFloat(price.toFixed(2));
     }
 
@@ -104,7 +104,7 @@ const Cart = () => {
                                                 </div>
                                             </Link>
                                             <div className="flex gap-1 pr-2">
-                                                <span className="pr-1">₹{item.price / 100}</span>
+                                                <span className="pr-1">₹{item.price === 0 ? item.defaultPrice / 100 : item.price / 100}</span>
                                                 <span className="cursor-pointer" onClick={() => removeFromCart(item)}>
                                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                                                         <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z" clipRule="evenodd" />
