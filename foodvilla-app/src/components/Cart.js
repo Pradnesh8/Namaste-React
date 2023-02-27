9689befca0e493cf2f34560d3f4a774f867b5bf3
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-02-28 00:54:19 +0530

    Show loader while loading images

--- foodvilla-app/src/components/Cart.js
+++ foodvilla-app/src/components/Cart.js
@@ -6,6 +6,24 @@ import emptycart from '../../assets/emptycart.png';
 import { CDN_IMG_URL } from "../config";
 import { useNavigate, useMatch } from "react-router-dom";
 import { clearCart } from "../utils/cartSlice";
+import { useState } from "react";
+const ImageRenderer = ({ item }) => {
+    const [loading, setLoading] = useState(true);
+    return (
+        <>
+            <div style={{ display: loading ? "block" : "none" }} className=" h-[5rem] w-[5rem] bg-gray-200 animate-pulse">
+            </div>
+            <div style={{ display: loading ? "none" : "block" }}>
+                <img
+                    className=" h-[5rem] w-[5rem] object-fill object-center"
+                    src={CDN_IMG_URL + item?.cloudinaryImageId}
+                    alt="dish-image"
+                    onLoad={() => setLoading(false)}
+                />
+            </div>
+        </>
+    )
+}
 const Cart = () => {
     const isCheckout = useMatch("/checkout");
     const cartItems = useSelector(store => store.cart.items);
@@ -59,9 +77,13 @@ const Cart = () => {
                                                     <div className="h-fit relative">
                                                         {
                                                             item?.cloudinaryImageId ?
-                                                                <img className=" h-[5rem] w-[5rem] object-cover object-center" src={CDN_IMG_URL + item.cloudinaryImageId} alt="dish-image" />
+                                                                <ImageRenderer item={item} />
                                                                 :
-                                                                <div className="bg-gray-200 h-[5rem] w-[5rem]"></div>
+                                                                <div className="bg-gray-200 h-[5rem] w-[5rem] flex justify-center items-center">
+                                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                                                        <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+                                                                    </svg>
+                                                                </div>
                                                         }
                                                         {
                                                             item.isVeg ?
