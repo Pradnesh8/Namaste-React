f15eab8e25e2383e1d359dcbe6a47b8b50ae005c
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-10 01:04:37 +0530

    Minor UI changes

--- Youtube-clone/youtube-clone/src/components/TabBar.js
+++ Youtube-clone/youtube-clone/src/components/TabBar.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const TabBar = () => {
     return (
-        <div className='my-3 mx-2 flex gap-3'>
+        <div className='py-3 px-2 flex gap-3 w-full fixed bg-white'>
             <button className='px-3 py-1 bg-black text-white rounded-md text-sm'>All</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>Comedy</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>Music</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>Thrillers</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>Gaming</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>JavaScript</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm'>Programming</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Comedy</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Music</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Thrillers</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Gaming</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>JavaScript</button>
+            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Programming</button>
         </div>
     )
 }
