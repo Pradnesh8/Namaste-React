fc0b990dcbaf4a212f12ce40152e46226270f9e6
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-10 01:04:26 +0530

    Added shimmer UI component

---
+++ Youtube-clone/youtube-clone/src/components/Shimmer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Shimmer = () => {
+    return (
+        <div className='mt-16 w-full flex flex-wrap justify-around gap-4'>
+            {
+                Array(30).fill("").map((item, index) => {
+                    return (
+                        <div className='flex flex-col gap-1'>
+                            <div key={index} className='w-72 h-40 bg-gray-200 animate-pulse'>
+                            </div>
+                            <div className='flex justify-start mt-1 items-start w-72 px-1'>
+                                <div className='w-8 h-8 mt-1 mr-3 rounded-full bg-gray-200 animate-pulse'></div>
+                                <div className='flex flex-col gap-1'>
+                                    <span className='w-48 h-2 rounded-xl animate-pulse bg-gray-200'></span>
+                                    <span className='w-32 h-2 rounded-xl animate-pulse bg-gray-200'></span>
+                                    <span className='w-32 h-2 rounded-xl animate-pulse bg-gray-200'></span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div >
+    )
+}
+
+export default Shimmer;
