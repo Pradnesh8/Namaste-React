c818bef69eb7de99f0807330b8d457a758d2a2fd
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-10 01:05:22 +0530

    URL for channel img

--- Youtube-clone/youtube-clone/src/utils/config.js
+++ Youtube-clone/youtube-clone/src/utils/config.js
@@ -2,3 +2,4 @@ const GOOGLE_API_KEY = "AIzaSyBVEgwMUuOmylGPjdHC7oUrxKtwRZNBNYM";
 export const YOUTUBE_VIDEOS_API =
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
     GOOGLE_API_KEY;
+export const YOUTUBE_CHANNEL_IMG_API = "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails&key=" + GOOGLE_API_KEY + "&id="
