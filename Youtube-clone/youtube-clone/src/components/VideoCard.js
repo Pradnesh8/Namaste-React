550769d1061256ad6b5f1cb002c07180c688b43b
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-10 01:04:58 +0530

    Added function to retrieve Channel image

--- Youtube-clone/youtube-clone/src/components/VideoCard.js
+++ Youtube-clone/youtube-clone/src/components/VideoCard.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { YOUTUBE_CHANNEL_IMG_API } from '../utils/config';
 import { convertToInternationalCurrencySystem } from '../utils/helper';
 
 const VideoCard = ({ info }) => {
     const { snippet, statistics } = info;
-    const { title, channelTitle, thumbnails } = snippet;
+    const { title, channelTitle, thumbnails, channelId } = snippet;
+    const [channelImg, setChannelImg] = useState("");
+    const getChannelImg = async () => {
+        const data = await fetch(YOUTUBE_CHANNEL_IMG_API + channelId);
+        const json = await data.json();
+        setChannelImg(json?.items[0]?.snippet?.thumbnails?.default?.url)
+    }
+    useEffect(() => {
+        getChannelImg();
+    }, [])
     return (
-        <div className='flex flex-col min-w-fit h-auto gap-1'>
-            <img src={thumbnails.medium.url} alt="thumbnail" className='w-72 rounded-lg' />
+        <div className='flex flex-col min-w-fit h-auto gap-1 cursor-pointer'>
+            <img src={thumbnails?.medium?.url} alt="thumbnail" className='w-72 rounded-lg' />
             <div className='flex justify-start mt-1 items-start w-72 px-1'>
-                <img src={thumbnails.default.url} alt="channel-image" className='w-8 h-8 mt-1 mr-3 rounded-full' />
+                <img src={channelImg} alt="channel-image" className='w-8 h-8 mt-1 mr-3 rounded-full' />
                 <div className='flex flex-col gap-[1px]'>
                     <span className='font-semibold'>{title}</span>
                     <span className='text-sm font-light'>{channelTitle}</span>
