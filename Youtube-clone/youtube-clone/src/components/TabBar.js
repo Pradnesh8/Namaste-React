58a2ba7d9e36321496b326e65ca5b7c6781938d2
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-12 01:35:56 +0530

    allTabs view

--- Youtube-clone/youtube-clone/src/components/TabBar.js
+++ Youtube-clone/youtube-clone/src/components/TabBar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const TabBar = () => {
+    const allTabs = ['Music', 'Gaming', 'News', 'Dramedy', 'Memes', 'Comedy', 'Movie musicals', 'Gadgets', 'Science', 'Action-adventure games', 'Cars', 'Recently uploaded']
     return (
-        <div className='py-3 px-2 flex gap-3 w-full fixed bg-white'>
+        <div className='py-3 px-2 flex gap-3 w-full fixed bg-white overflow-hidden'>
             <button className='px-3 py-1 bg-black text-white rounded-md text-sm'>All</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Comedy</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Music</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Thrillers</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Gaming</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>JavaScript</button>
-            <button className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>Programming</button>
+            {
+                allTabs.map((tab) => {
+                    return <button key={crypto.randomUUID()} className='px-3 py-1 bg-gray-100 rounded-md text-sm hover:bg-gray-200'>{tab}</button>
+                })
+            }
         </div>
     )
 }
