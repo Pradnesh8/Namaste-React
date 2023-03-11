2dc6a9d9cbc65fb47dbcd988635f607be7f98d07
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-12 01:33:57 +0530

    Moment library to find time of upload of video

--- Youtube-clone/youtube-clone/src/utils/helper.js
+++ Youtube-clone/youtube-clone/src/utils/helper.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 export const convertToInternationalCurrencySystem = (labelValue) => {
     // Nine Zeroes for Billions
     return Math.abs(Number(labelValue)) >= 1.0e+9
@@ -9,4 +10,31 @@ export const convertToInternationalCurrencySystem = (labelValue) => {
             : Math.abs(Number(labelValue)) >= 1.0e+3
                 ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(2) + "K"
                 : Math.abs(Number(labelValue));
+}
+
+export const getTimeDifference = (inputDate) => {
+    const current_time = moment(); // current time
+    const other_time = moment(inputDate); // other time
+
+    const duration = moment.duration(current_time.diff(other_time)); // time difference as duration
+
+    const day_diff = duration.asDays(); // time difference in days
+    const month_diff = duration.asMonths(); // time difference in months
+    const year_diff = duration.asYears(); // time difference in years
+
+    const day_difference = Math.round(day_diff);
+    const month_difference = Math.round(month_diff);
+    const year_difference = Math.round(year_diff);
+
+    if (year_difference > 0) {
+        return `${year_difference} ${year_difference > 1 ? "years" : "year"} ago`
+    }
+    else if (month_difference > 0) {
+        return `${month_difference} ${month_difference > 1 ? "months" : "month"} ago`
+    }
+    else if (day_difference > 0) {
+        return `${day_difference} ${day_difference > 1 ? "days" : "day"} ago`
+    } else {
+        return `1 day ago`
+    }
 }
