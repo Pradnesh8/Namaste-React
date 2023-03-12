4c8ac4abb1c852f9890b1f68252a4a8841033bcb
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-13 00:36:29 +0530

    search content API

--- Youtube-clone/youtube-clone/src/utils/config.js
+++ Youtube-clone/youtube-clone/src/utils/config.js
@@ -5,3 +5,4 @@ export const YOUTUBE_VIDEOS_API =
 export const YOUTUBE_CHANNEL_IMG_API = "https://www.googleapis.com/youtube/v3/channels?part=snippet&fields=items%2Fsnippet%2Fthumbnails&key=" + GOOGLE_API_KEY + "&id="
 export const GOOGLE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 export const YOUTUBE_VIDEO_LIST_SEARCH_API = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=" + GOOGLE_API_KEY + "&q="
+export const YOUTUBE_VIDEO_LIST_SEARCH_CONTENT_API = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics&key=" + GOOGLE_API_KEY + "&id="
