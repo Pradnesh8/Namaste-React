c7078e1273b64f0becd59cdcc8f4ad8dcf832d67
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-17 01:00:47 +0530

    error comp

---
+++ Youtube-clone/youtube-clone/src/components/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import dog from '../assets/error-dog.jpg';
+const ErrorPage = () => {
+    return (
+        <div className='flex flex-col justify-center items-center flex-[6]'>
+            <div className='text-4xl font-light'>Oops! Something went wrong</div>
+            <div className='text-xl font-light'>Please try again later!</div>
+            <img src={dog} alt="error" className='h-[60vh] mt-3 w-auto rounded-md' />
+        </div>
+    )
+}
+
+export default ErrorPage
