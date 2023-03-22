4853039079dc597a3234931190ec91814ab00a81
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-23 00:35:47 +0530

    OnButton Click event added for Search button

--- Youtube-clone/youtube-clone/src/components/Navbar.js
+++ Youtube-clone/youtube-clone/src/components/Navbar.js
@@ -58,7 +58,10 @@ const Navbar = () => {
                         }, 300)
                     }} id="search-bar" placeholder='Search' className='p-2 pl-5 w-[35vw] border rounded-l-3xl rounded-r-none placeholder:font-normal placeholder:text-gray-500' value={searchQuery} />
                 </span>
-                <button className='px-5 py-2 border bg-gray-100 rounded-r-3xl rounded-l-none'>
+                <button className='px-5 py-2 border bg-gray-100 rounded-r-3xl rounded-l-none' onClick={() => {
+                    setSearchQuery(searchQuery);
+                    navigate("/results?search_query=" + searchQuery)
+                }}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
