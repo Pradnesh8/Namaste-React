1dbbc6ee81c6da456a872afdfd3ceea859cde692
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-23 02:11:09 +0530

    removed keys

--- Youtube-clone/youtube-clone/src/utils/config.js
+++ Youtube-clone/youtube-clone/src/utils/config.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBVEgwMUuOmylGPjdHC7oUrxKtwRZNBNYM";
+const GOOGLE_API_KEY = process.env.YTB_API_KEY;
 export const YOUTUBE_VIDEOS_API =
     "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
     GOOGLE_API_KEY;
