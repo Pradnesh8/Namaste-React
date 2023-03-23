86426dcc5805ff0580c2d5ffc90886d7fbc0ac9e
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-03-24 01:06:33 +0530

    UI minor changes

--- Youtube-clone/youtube-clone/src/components/Navbar.js
+++ Youtube-clone/youtube-clone/src/components/Navbar.js
@@ -91,12 +91,13 @@ const Navbar = () => {
             </div>
 
             <div className='notify-login flex justify-end mr-6 items-center gap-5'>
-                <span className='notifiaction-bell'>
+                <span className='notifiaction-bell relative cursor-pointer mt-1' title='Notifications'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-7 h-7">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
                     </svg>
+                    <span className='absolute -top-2 -right-1 p-1 h-5 w-5 flex justify-center items-center rounded-full text-xs bg-red-600 text-white'>8</span>
                 </span>
-                <img src={profilePhoto} alt="Profile" className='w-10 h-10 rounded-full' />
+                <img src={profilePhoto} title="Profile" alt="Profile" className='w-10 h-10 rounded-full cursor-pointer' />
             </div>
         </section>
     )
