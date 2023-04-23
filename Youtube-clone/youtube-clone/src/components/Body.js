84c8bf943c24ffd253d7b1c76fe5de6208ce1c87
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2023-04-23 16:59:49 +0530

    fetching category in Body

--- Youtube-clone/youtube-clone/src/components/Body.js
+++ Youtube-clone/youtube-clone/src/components/Body.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import TabBar from './TabBar';
 import VideoContainer from './VideoContainer';
+import { useSelector } from 'react-redux';
 
 const Body = () => {
+    const videoCategoryMain = useSelector(store => store.app.selectedCategory);
+    useEffect(() => {
+        console.log("changed category", videoCategoryMain);
+    }, [videoCategoryMain])
     return (
         <div className='mt-12 overflow-x-hidden md:mx-3 md:flex-[6]'>
             <TabBar />
-            <VideoContainer />
+            <VideoContainer category={videoCategoryMain} />
         </div>
     )
 }
